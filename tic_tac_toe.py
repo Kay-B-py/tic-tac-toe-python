b932a187c6c102f1ee4919b4f325a61a6b7f03eb
Kay-B-py <kay.1405.2026@gmail.com>
2026-09-13 13:03:35 +0200

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,83 +1,92 @@
-def display_board(board): #ale letztes nur visuell
-    trennzeile = "+-------+-------+-------+"
-    zw_trennzeile = "|       |       |       |"
-    print(trennzeile)
+from random import randrange
+EMPTY = "-"
+ 
+ 
+def display_board(board):
+    border_line = "+-------+-------+-------+"
+    spacer_line = "|       |       |       |"
+    print(border_line)
     for row in range(len(board)):
-        inhaltszeile = f"|   {board[row][0]}   |   {board[row][1]}   |   {board[row][2]}   |"
-        print(zw_trennzeile)
-        print(inhaltszeile)
-        print(zw_trennzeile)
-        print(trennzeile)
-
-        
+        row_line = f"|   {board[row][0]}   |   {board[row][1]}   |   {board[row][2]}   |"
+        print(spacer_line)
+        print(row_line)
+        print(spacer_line)
+        print(border_line)
+ 
+ 
 def enter_move(board):
-    new_move = int(input("Show me your next move: "))
-    if new_move > 9 or new_move < 1:
+    player_choice = int(input("Show me your next move: "))
+    if player_choice > 9 or player_choice < 1:
         print("This value is not choosable")
         return enter_move(board)
-    elif board[(new_move-1)//3][(new_move -1) % 3] == 'X' or board[(new_move-1)//3][(new_move -1) % 3] == 'O':
+    elif board[(player_choice-1)//3][(player_choice -1) % 3] == 'X' or board[(player_choice-1)//3][(player_choice -1) % 3] == 'O':
         print("You cant choose this value")
         return enter_move(board)
     else:
-        board[(new_move-1)//3][(new_move -1) % 3] = 'O'
+        board[(player_choice-1)//3][(player_choice -1) % 3] = 'O'
         return board
-
-
+ 
+ 
 def make_list_of_free_fields(board):
-    tuple_board = []
+    open_spots = []
     for i in range (9):
         if board[(i)//3][(i) % 3] == EMPTY:
-            tuple_board.append(((i)//3,(i) % 3))
-    return tuple_board
-
-
-
+            open_spots.append(((i)//3,(i) % 3))
+    return open_spots
+ 
+ 
+ 
 def victory_for(board, sign):
-    alle_linien = [
-    [(0,0),(0,1),(0,2)],   # Zeile 0
-    [(1,0),(1,1),(1,2)],   # Zeile 1
-    [(2,0),(2,1),(2,2)],   # Zeile 2
-    [(0,0),(1,0),(2,0)],   # Spalte 0
-    [(0,1),(1,1),(2,1)],   # Spalte 1
-    [(0,2),(1,2),(2,2)],   # Spalte 2
-    [(0,0),(1,1),(2,2)],   # Diagonale
-    [(0,2),(1,1),(2,0)],   # Diagonale
+    winning_lines = [
+    [(0,0),(0,1),(0,2)],   # row 0
+    [(1,0),(1,1),(1,2)],   # row 1
+    [(2,0),(2,1),(2,2)],   # row 2
+    [(0,0),(1,0),(2,0)],   # column 0
+    [(0,1),(1,1),(2,1)],   # column 1
+    [(0,2),(1,2),(2,2)],   # column 2
+    [(0,0),(1,1),(2,2)],   # diagonal
+    [(0,2),(1,1),(2,0)],   # diagonal
 ]
-    for linie in alle_linien:
-        treffer = 0
-        for koordinate in linie:
-            if board[koordinate[0]][koordinate[1]] == sign:
-                treffer = treffer + 1
-        if treffer == 3:
+    for line in winning_lines:
+        hits = 0
+        for cell in line:
+            if board[cell[0]][cell[1]] == sign:
+                hits = hits + 1
+        if hits == 3:
             return True
     return False
-
-    
-        
-    # the player using 'O's or 'X's has won the game
-
-
+ 
+ 
 def draw_move(board):
-    freie_felder = make_list_of_free_fields(board)
-    pc_move =(randrange(len(freie_felder)))
-    board[freie_felder[pc_move][0]][freie_felder[pc_move][1]] = 'X'
+    open_spots = make_list_of_free_fields(board)
+    for spot in open_spots:
+        board[spot[0]][spot[1]] = 'X'
+        if victory_for(board, 'X'):
+            return board
+        else:
+            board[spot[0]][spot[1]] = EMPTY
+ 
+    for spot in open_spots:
+        board[spot[0]][spot[1]] = 'O'
+        if victory_for(board, 'O'):
+            board[spot[0]][spot[1]] = 'X'
+            return board
+        else:
+            board[spot[0]][spot[1]] = EMPTY
+    computer_choice =(randrange(len(open_spots)))
+    board[open_spots[computer_choice][0]][open_spots[computer_choice][1]] = 'X'   
     return board
-
-
-
-
-#The programm itself, the concept:
-from random import randrange
-EMPTY = "-"
-def spiel_starten():
+ 
+ 
+def start_game():
     print("Welcome to a game of a tic-tac-toe, are you ready to face this enemy?")
     print("""Your enemy gonna uses the X's, while youre using the O's
             Your enemy starts with his first move""")
-    #sign soll "X" oder "O" sein
-    board = [[EMPTY for row in range(3)]for column in range(3)] #Empty noch nicht definiert (was ist Empty überhaupt?
-    #Ich muss die Zeilen den Zahlen zuordnen bzw. festlegen count = number
+    # sign should be "X" or "O"
+    board = [[EMPTY for row in range(3)]for column in range(3)]
     board[1][1] = 'X'
     count = 1
+    display_board(board)
     while count != 9:
         board = enter_move(board)
         count += 1 
@@ -92,15 +101,19 @@ def spiel_starten():
         if victory_for(board, 'X'):
             print("The player using X's is the winner")
             break
-    if count == 9:
+    else:
         print("No Winners here")
-
-spiel_starten()
+ 
+ 
+ 
+ 
+# The program itself, the concept:
+start_game()
 answer = input("The game ends, wanna restart, type Yes for a restart and No for game to end")
-while answer == 'Yes':
-    spiel_starten()
+yes_responses = ["yes", "y"]
+while answer.lower() in yes_responses:
+    start_game()
     answer = input("The game ends, wanna restart, type Yes for a restart and No for game to end")
-    if answer == 'No':
-        break
-
+ 
+ 
 print("Thank you for playing")
